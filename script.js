a392e269b186e7533ca89f719e55e16ed33e4a09
aradhana singh <113791942+gitian000@users.noreply.github.com>
2024-03-19 00:55:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+let expenses =[];
+let totalAmount = 0;
+
+const categorySelect = document.getElementById('category-select')
+const amountInput = document.getElementById('amount-input')
+const addBtn = document.getElementById('add-btn')
+const expenseTableBody = document.getElementById('expense-table-body')
+const totalAmountCell = document.getElementById('total-amount')
+
+addBtn.addEventListener('click', function(){
+    const category = categorySelect.value;
+    const amount = Number(amountInput.value);
+
+    if(category === ''){
+        alert('please select a category');
+        return;
+    }
+    if(isNaN(amount) || amount <= 0){
+        alert('please enter a valid amount');
+        return;
+    }
+    
+    expenses.push({category,amount,date})
+
+    totalAmount += amount;
+    totalAmountCell.textContent=totalAmount;
+
+    const newRow = expenseTableBody.insertRow();
+
+    const categoryCell = newRow.insertCell();
+    const AmountCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function(){
+        expenses.splice(expenses.indexOf(expense),1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expenseTableBody.removeChild(newRow);
+    })
+    const expense= expenses[expenses.length - 1];
+    categoryCell.textContent=expense.category;
+    AmountCell.textContent=expense.amount;
+    deleteCell.appendChild(deleteBtn);
+
+});
+for(const exepense of expenses){
+    totalAmount += amount;
+    totalAmountCell.textContent=totalAmount;
+
+    const newRow = expenseTableBody.insertRow();
+
+    const categoryCell = newRow.insertCell();
+    const AmountCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function(){
+        expenses.splioce(expenses.indexOf(expense),1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expenseTableBody.removeChild(newRow);
+    })
+    const expense= expenses[expenses.length - 1];
+    categoryCell.textContent=expense.category;
+    AmountCell.textContent=expense.amount;
+    deleteCell.appendChild(deleteBtn);
+}
